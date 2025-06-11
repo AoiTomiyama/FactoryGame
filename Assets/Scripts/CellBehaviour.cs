c3fb0e3e85964dc201e70fa59341f4a0e5a55b7c
AoiTomiyama <TadoO.o2209@gmail.com>
2025-06-11 16:48:17 +0900

    [add] グリッド情報をScriptableObjectとして保存するようにした

---
+++ Assets/Scripts/CellBehaviour.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class CellBehaviour : MonoBehaviour
+{
+    public int xIndex { get; set; }
+    public int zIndex { get; set; }
+}
--- Assets/Scripts/GridFieldGenerator.cs
+++ Assets/Scripts/GridFieldGenerator.cs
@@ -2,6 +2,21 @@ using UnityEngine;
 
 public class GridFieldGenerator : MonoBehaviour
 {
+    [SerializeField] private GridFieldSO _fieldSo;
     public GameObject cellPrefab;
     public int gridSize = 30;
+
+    private void Start()
+    {
+        _fieldSo.GridCells = new GameObject[gridSize, gridSize];
+        for (int x = 0; x < gridSize; x++)
+        {
+            var separator = transform.GetChild(x);
+            for (int z = 0; z < gridSize; z++)
+            {
+                var cell = separator.GetChild(z).gameObject;
+                _fieldSo.SetNewCell(x, z, cell);
+            }
+        }
+    }
 }
