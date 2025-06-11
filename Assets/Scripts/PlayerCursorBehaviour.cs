722c635aa7c7a822b303c8b06c2ab037be1eefae
AoiTomiyama <TadoO.o2209@gmail.com>
2025-06-11 16:49:40 +0900

    [add] 選択セルを置き換える機能追加。

--- Assets/Scripts/PlayerCursorBehaviour.cs
+++ Assets/Scripts/PlayerCursorBehaviour.cs
@@ -1,52 +1,104 @@
-using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(PlayerInput))]
 public class PlayerCursorBehaviour : MonoBehaviour
 {
     [SerializeField] private LayerMask _layerMask;
+    [SerializeField] private Color _selectedColor = Color.red;
+    [SerializeField] private InputAction _mouseAction;
+    [SerializeField] private InputAction _leftClickAction;
+    [SerializeField] private InputAction _rightClickAction;
+    [SerializeField] private GameObject _cellPrefab;
+    [SerializeField] private GridFieldSO _fieldSo;
     private Camera _camera;
-    private InputAction _mouseAction;
-    private InputAction _clickAction;
+    private GameObject _selectedObject;
 
     private void Start()
     {
         _camera = Camera.main;
-        _mouseAction = new InputAction("PlayerActions", binding: "<Mouse>/position");
+    }
+
+    private void OnEnable()
+    {
         _mouseAction.Enable();
-        _mouseAction.performed += MouseMove;
-        
-        _clickAction = new InputAction("PlayerActions", binding: "<Mouse>/leftButton");
-        _clickAction.Enable();
-        _clickAction.performed += Click;
+        _leftClickAction.Enable();
+        _rightClickAction.Enable();
+
+        _mouseAction.performed += OnMouseMove;
+        _leftClickAction.performed += OnLeftClick;
+        _rightClickAction.performed += OnRightClick;
     }
 
     private void OnDisable()
     {
         _mouseAction.Disable();
-        _clickAction.Disable();
+        _leftClickAction.Disable();
+        _rightClickAction.Disable();
     }
 
-    private void MouseMove(InputAction.CallbackContext context)
+    private void OnMouseMove(InputAction.CallbackContext context)
     {
-        
+        if (!context.performed) return;
+
         var ray = _camera.ScreenPointToRay(context.ReadValue<Vector2>());
         if (Physics.Raycast(ray, out var hit, Mathf.Infinity, _layerMask))
         {
-            var otherPos = hit.collider.gameObject.transform.position;
-            transform.position = otherPos + Vector3.up;
+            // 直前に選択されていたオブジェクトと同じ場合は何もしない
+            if (hit.collider.gameObject == _selectedObject) return;
+
+            SelectGrid(hit.collider.gameObject);
         }
     }
-    
-    private void Click(InputAction.CallbackContext context)
+
+    private void SelectGrid(GameObject target)
+    {
+        if (target == null) return;
+
+        // 直前に選択されていたオブジェクトがある場合、その色を元に戻す
+        if (_selectedObject != null &&
+            _selectedObject.TryGetComponent<Renderer>(out var prevRenderer))
+        {
+            prevRenderer.material.color = Color.white;
+        }
+
+        _selectedObject = target;
+        if (_selectedObject.TryGetComponent<Renderer>(out var currentRenderer))
+        {
+            currentRenderer.material.color = _selectedColor;
+        }
+    }
+
+    private void OnLeftClick(InputAction.CallbackContext context)
     {
         if (!context.performed) return;
-        var ray = _camera.ScreenPointToRay(_mouseAction.ReadValue<Vector2>());
-        if (Physics.Raycast(ray, out var hit, Mathf.Infinity, _layerMask))
+
+        if (_selectedObject.TryGetComponent<CellBehaviour>(out var cellBehaviour))
         {
-            Debug.Log($"Clicked on: {hit.collider.gameObject.name}");
-            // ここでクリック時の処理を追加できます
+            var x = cellBehaviour.xIndex;
+            var z = cellBehaviour.zIndex;
+            ReplaceCell(x, z);
+        }
+        else
+        {
+            Debug.LogError($"{typeof(CellBehaviour)}が未割り当てです。");
         }
     }
+
+    /// <summary>
+    /// 選択されているセルを新しいセルに置き換える
+    /// </summary>
+    private void ReplaceCell(int x, int z)
+    {
+        var objName = _selectedObject.name;
+        var pos = _selectedObject.transform.position;
+        var parent = _selectedObject.transform.parent;
+        Destroy(_selectedObject);
+        
+        var newObj = Instantiate(_cellPrefab, pos, Quaternion.identity, parent);
+        newObj.name = objName;
+        _fieldSo.SetNewCell(x, z, newObj);
+        SelectGrid(newObj);
+    }
+
+    private void OnRightClick(InputAction.CallbackContext context) {}
 }
