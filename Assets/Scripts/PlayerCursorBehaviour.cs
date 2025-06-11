645cb1a1edf6318c5bf87b8c2652726b40d2348f
AoiTomiyama <TadoO.o2209@gmail.com>
2025-06-11 11:55:57 +0900

    [change] 入力の登録を全てスクリプト上で行うように変更

--- Assets/Scripts/PlayerCursorBehaviour.cs
+++ Assets/Scripts/PlayerCursorBehaviour.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,13 +7,28 @@ public class PlayerCursorBehaviour : MonoBehaviour
 {
     [SerializeField] private LayerMask _layerMask;
     private Camera _camera;
+    private InputAction _mouseAction;
+    private InputAction _clickAction;
 
     private void Start()
     {
         _camera = Camera.main;
+        _mouseAction = new InputAction("PlayerActions", binding: "<Mouse>/position");
+        _mouseAction.Enable();
+        _mouseAction.performed += MouseMove;
+        
+        _clickAction = new InputAction("PlayerActions", binding: "<Mouse>/leftButton");
+        _clickAction.Enable();
+        _clickAction.performed += Click;
     }
 
-    public void MouseMove(InputAction.CallbackContext context)
+    private void OnDisable()
+    {
+        _mouseAction.Disable();
+        _clickAction.Disable();
+    }
+
+    private void MouseMove(InputAction.CallbackContext context)
     {
         
         var ray = _camera.ScreenPointToRay(context.ReadValue<Vector2>());
@@ -20,6 +36,17 @@ public class PlayerCursorBehaviour : MonoBehaviour
         {
             var otherPos = hit.collider.gameObject.transform.position;
             transform.position = otherPos + Vector3.up;
+        }
+    }
+    
+    private void Click(InputAction.CallbackContext context)
+    {
+        if (!context.performed) return;
+        var ray = _camera.ScreenPointToRay(_mouseAction.ReadValue<Vector2>());
+        if (Physics.Raycast(ray, out var hit, Mathf.Infinity, _layerMask))
+        {
+            Debug.Log($"Clicked on: {hit.collider.gameObject.name}");
+            // ここでクリック時の処理を追加できます
         }
     }
 }
