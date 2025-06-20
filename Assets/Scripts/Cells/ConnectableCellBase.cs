09ccd09c4e981c98f932d2f00cb2e58341d772b7
AoiTomiyama <TadoO.o2209@gmail.com>
2025-06-21 00:41:24 +0900

    [change] 隣接セルの取得ロジックを最適化。

--- Assets/Scripts/Cells/ConnectableCellBase.cs
+++ Assets/Scripts/Cells/ConnectableCellBase.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEngine;
 
 public abstract class ConnectableCellBase : CellBase
 {
@@ -17,26 +18,45 @@ public abstract class ConnectableCellBase : CellBase
     private void ConnectAdjacentCells(ConnectableCellBase fromCell)
     {
         // 自分自身を除外リストに追加
-        var excludingList = new List<CellBase> { this };
+        var excludingList = new List<CellBase>(AdjacentCells) { this };
 
         // 周囲1マス以内のセルを取得
         for (int i = 0; i < AdjacentCount; i++)
         {
-            if (!GridFieldDatabase.Instance.TryGetCellFromRange(XIndex, ZIndex, 1, out AdjacentCells[i],
+            if (!GridFieldDatabase.Instance.TryGetCellFromRange(XIndex, ZIndex, 1, out var foundCell,
                     excludingList)) continue;
-
-            excludingList.Add(AdjacentCells[i]);
+            
+            // 取得できたセルを除外リストに追加
+            excludingList.Add(foundCell);
+            
+            // 取得できたセルがEmptyCellであればスキップ
+            if (foundCell is EmptyCell) continue;
+            
+            // 取得できたセルをAdjacentCellsに追加
+            AdjacentCells[i] = foundCell;
 
             // 取得できたセルがConnectableCellBaseのであれば、接続を行う
-            if (AdjacentCells[i] is not ConnectableCellBase connectableCell) continue;
+            if (foundCell is not ConnectableCellBase connectableCell) continue;
 
             // 接続先セルのAdjacentCellsに接続元のセルがなければ追加
             if (connectableCell.AdjacentCells.Contains(fromCell)) continue;
             
-            var index = Array.IndexOf(connectableCell.AdjacentCells, 
-                connectableCell.AdjacentCells.FirstOrDefault(cell => cell.XIndex == fromCell.XIndex && cell.ZIndex == fromCell.ZIndex));
-                
-            connectableCell.AdjacentCells[index] = fromCell;
+            // 向こうのセルのAdjacentCellsに接続元のセルを追加
+            connectableCell.ConnectAdjacentCells(fromCell);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (AdjacentCells == null || AdjacentCells.Length == 0) return;
+
+        // 接続表示（デバッグ用）
+        Gizmos.color = Color.green;
+        var startPadding = Vector3.up * 3f;
+        var endPadding = Vector3.up * 4f;
+        foreach (var cell in AdjacentCells.Where(cell => cell != null))
+        {
+            Gizmos.DrawLine(transform.position + startPadding, cell.transform.position + endPadding);
         }
     }
 }
