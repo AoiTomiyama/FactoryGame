e8e7519bc6b67b3a094a788aa307407904ad60b2
AoiTomiyama <TadoO.o2209@gmail.com>
2025-06-20 22:59:24 +0900

    [fix] セルの接続を再帰的に呼び出してしまう不具合修正

--- Assets/Scripts/Cells/ConnectableCellBase.cs
+++ Assets/Scripts/Cells/ConnectableCellBase.cs
@@ -1,29 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 public abstract class ConnectableCellBase : CellBase
 {
-    protected CellBase[] AdjacentCells { get; set; }
+    protected CellBase[] AdjacentCells { get; private set; }
+    private const int AdjacentCount = 4;
 
-    protected override void Awake()
+    protected virtual void Start()
     {
-        base.Awake();
-        ConnectAdjacentCells();
+        AdjacentCells = new CellBase[AdjacentCount];
+
+        ConnectAdjacentCells(this);
     }
 
-    private void ConnectAdjacentCells()
+    private void ConnectAdjacentCells(ConnectableCellBase fromCell)
     {
-        const int adjacentCount = 4;
-        AdjacentCells = new CellBase[adjacentCount];
+        // 自分自身を除外リストに追加
+        var excludingList = new List<CellBase> { this };
 
         // 周囲1マス以内のセルを取得
-        for (int i = 0; i < adjacentCount; i++)
+        for (int i = 0; i < AdjacentCount; i++)
         {
             if (!GridFieldDatabase.Instance.TryGetCellFromRange(XIndex, ZIndex, 1, out AdjacentCells[i],
-                    AdjacentCells)) continue;
-            
+                    excludingList)) continue;
+
+            excludingList.Add(AdjacentCells[i]);
+
             // 取得できたセルがConnectableCellBaseのであれば、接続を行う
-            if (AdjacentCells[i] is ConnectableCellBase connectableCell)
-            {
-                connectableCell.ConnectAdjacentCells();
-            }
+            if (AdjacentCells[i] is not ConnectableCellBase connectableCell) continue;
+
+            // 接続先セルのAdjacentCellsに接続元のセルがなければ追加
+            if (connectableCell.AdjacentCells.Contains(fromCell)) continue;
+            
+            var index = Array.IndexOf(connectableCell.AdjacentCells, 
+                connectableCell.AdjacentCells.FirstOrDefault(cell => cell.XIndex == fromCell.XIndex && cell.ZIndex == fromCell.ZIndex));
+                
+            connectableCell.AdjacentCells[index] = fromCell;
         }
     }
 }
