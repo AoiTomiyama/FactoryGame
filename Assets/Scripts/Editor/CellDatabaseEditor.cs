98273c4a9b4511b8e06f3529a6801991ff8f0038
AoiTomiyama <TadoO.o2209@gmail.com>
2025-06-27 19:06:53 +0900

    [change] 自動割り当てをEditorファイルに移動

--- Assets/Scripts/Editor/CellDatabaseEditor.cs
+++ Assets/Scripts/Editor/CellDatabaseEditor.cs
@@ -1,9 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
 [CustomEditor(typeof(CellDatabaseSO))]
 public class CellDatabaseEditor : Editor
 {
+    // ファイルのパスやPrefabのプレフィックス
+    private const string PrefabFolderPath = "Assets/Prefabs/";
+    private const string PlaceholderPrefabPrefix = "P_";
+    private const string PlaceholderFilePath = PrefabFolderPath + "Placeholder";
+    private const string FieldPrefabPrefix = "F_";
+    private const string FieldFilePath = PrefabFolderPath + "Field";
+    private const string Filter = "t:Prefab";
+
+    [SerializeField] private GameObject basePrefab;
+    [SerializeField] private GameObject modelPrefab;
+
     public override void OnInspectorGUI()
     {
         DrawDefaultInspector();
@@ -14,9 +29,68 @@ public class CellDatabaseEditor : Editor
         {
             cellDatabase.ValidateAndBuildLookup();
         }
+
         if (GUILayout.Button("Auto Assign"))
         {
-            cellDatabase.AutoAssignData();
+            AutoAssignData(cellDatabase);
         }
     }
+
+    private static void AutoAssignData(CellDatabaseSO database)
+    {
+        // "Assets/Prefabs" 以下の .prefab ファイルを全検索
+        var placeholders = AssetDatabase.FindAssets(Filter, new[] { PlaceholderFilePath });
+        var fields = AssetDatabase.FindAssets(Filter, new[] { FieldFilePath });
+
+        // 辞書へ登録
+        var fieldDict = new Dictionary<string, GameObject>();
+        foreach (var guid in fields)
+        {
+            var path = AssetDatabase.GUIDToAssetPath(guid);
+            var fileName = Path.GetFileNameWithoutExtension(path);
+
+            if (!fileName.StartsWith(FieldPrefabPrefix)) continue;
+            var key = fileName[FieldPrefabPrefix.Length..];
+            var prefab = AssetDatabase.LoadAssetAtPath<GameObject>(path);
+            fieldDict[key] = prefab;
+        }
+
+        var placeholderDict = new Dictionary<string, GameObject>();
+        foreach (var guid in placeholders)
+        {
+            var path = AssetDatabase.GUIDToAssetPath(guid);
+            var fileName = Path.GetFileNameWithoutExtension(path);
+
+            if (!fileName.StartsWith(PlaceholderPrefabPrefix)) continue;
+            var key = fileName[PlaceholderPrefabPrefix.Length..];
+            var prefab = AssetDatabase.LoadAssetAtPath<GameObject>(path);
+            placeholderDict[key] = prefab;
+        }
+
+        var length = Enum.GetNames(typeof(CellType)).Length;
+        var list = new CellInfo[length];
+        // 配列に割り当て
+        for (int i = 0; i < length; i++)
+        {
+            var cellTypeName = Enum.GetName(typeof(CellType), i) + "Cell";
+
+            if (fieldDict.TryGetValue(cellTypeName, out var fieldPrefab))
+            {
+                list[i].fieldCellPrefab = fieldPrefab;
+            }
+
+            if (placeholderDict.TryGetValue(cellTypeName, out var placeholderPrefab))
+            {
+                list[i].placeholderCellPrefab = placeholderPrefab;
+            }
+
+            list[i].cellType = (CellType)i;
+        }
+
+        database.SetCellInfos(list);
+        database.ValidateAndBuildLookup();
+
+        Debug.Log("AutoAssignData 完了");
+    }
+
 }
--- Assets/Scripts/ScriptableObject/CellDatabaseSO.cs
+++ Assets/Scripts/ScriptableObject/CellDatabaseSO.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
-using UnityEditor;
+using System.Linq;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "CellDatabaseSO", menuName = "Scriptable Objects/CellDatabaseSO")]
@@ -24,65 +23,9 @@ public class CellDatabaseSO : ScriptableObject
         ValidateAndBuildLookup();
     }
 
-    public void AutoAssignData()
-    {
-        const string PlaceholderPrefabPrefix = "P_";
-        const string PlaceholderFileName = "Placeholder";
-        const string FieldPrefabPrefix = "F_";
-        const string FieldFileName = "Field";
-        const string PrefabFolderName = "Assets/Prefabs/";
-        const string Filter = "t:Prefab";
-        
-        // "Assets/Prefabs" 以下の .prefab ファイルを全検索
-        var placeholders = AssetDatabase.FindAssets(Filter, new[] { PrefabFolderName + PlaceholderFileName });
-        var fields = AssetDatabase.FindAssets(Filter, new[] { PrefabFolderName + FieldFileName });
-
-        // 辞書へ登録
-        var fieldDict = new Dictionary<string, GameObject>();
-        foreach (var guid in fields)
-        {
-            var path = AssetDatabase.GUIDToAssetPath(guid);
-            var fileName = Path.GetFileNameWithoutExtension(path);
-            
-            if (!fileName.StartsWith(FieldPrefabPrefix)) continue;
-            var key = fileName[FieldPrefabPrefix.Length..];
-            var prefab = AssetDatabase.LoadAssetAtPath<GameObject>(path);
-            fieldDict[key] = prefab;
-        }
-
-        var placeholderDict = new Dictionary<string, GameObject>();
-        foreach (var guid in placeholders)
-        {
-            var path = AssetDatabase.GUIDToAssetPath(guid);
-            var fileName = Path.GetFileNameWithoutExtension(path);
-            
-            if (!fileName.StartsWith(PlaceholderPrefabPrefix)) continue;
-            var key = fileName[PlaceholderPrefabPrefix.Length..];
-            var prefab = AssetDatabase.LoadAssetAtPath<GameObject>(path);
-            placeholderDict[key] = prefab;
-        }
-
-        // 配列に割り当て
-        for (int i = 0; i < Enum.GetNames(typeof(CellType)).Length; i++)
-        {
-            var cellTypeName = Enum.GetName(typeof(CellType), i) + "Cell";
-
-            if (fieldDict.TryGetValue(cellTypeName, out var fieldPrefab))
-            {
-                cellPairingInfos[i].fieldCellPrefab = fieldPrefab;
-            }
-
-            if (placeholderDict.TryGetValue(cellTypeName, out var placeholderPrefab))
-            {
-                cellPairingInfos[i].placeholderCellPrefab = placeholderPrefab;
-            }
-
-            cellPairingInfos[i].cellType = (CellType)i;
-        }
-
-        Debug.Log("AutoAssignData 完了");
-    }
-
+    /// <summary>
+    /// 保存済みデータのヴァリデーション処理。
+    /// </summary>
     public void ValidateAndBuildLookup()
     {
         _infoLookup = new Dictionary<CellType, CellInfo>();
@@ -142,6 +85,11 @@ public class CellDatabaseSO : ScriptableObject
         }
 
         return new List<CellInfo>(_infoLookup.Values);
+    }
+
+    public void SetCellInfos(IEnumerable<CellInfo> cellInfos)
+    {
+        cellPairingInfos = cellInfos.ToArray();
     }
 }
 
