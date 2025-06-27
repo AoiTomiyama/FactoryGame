393398be325bd8ed08ceee0b5466d20d15535b00
AoiTomiyama <TadoO.o2209@gmail.com>
2025-06-27 22:27:23 +0900

    [add] レシピのScriptableObjectを追加

---
+++ Assets/Scripts/ScriptableObject/Recipes/RecipeDataSO.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "RecipeDataSO", menuName = "Scriptable Objects/RecipeDataSO")]
+public class RecipeDataSO : ScriptableObject
+{
+    [Serializable]
+    public struct Ingredient
+    {
+        public ResourceType resourceType;
+        public int requiredAmount;
+    }
+
+    [SerializeField] [Tooltip("必要となるリソース情報")]
+    private Ingredient[] ingredients;
+    
+    [SerializeField] [Tooltip("出来上がる成果物")]
+    private ResourceType result;
+    
+    [SerializeField] [Tooltip("成果物の個数")]
+    private int resultAmount;
+    
+    public Ingredient[] Ingredients => ingredients;
+    public ResourceType Result => result;
+    public int ResultAmount => resultAmount;
+}
