c19f6765bea86acd3eb37179d0c0e7edfa6162a4
AoiTomiyama <TadoO.o2209@gmail.com>
2025-07-14 23:52:43 +0900

    [add] リソースセルのプロバイダー対応

---
+++ Assets/Scripts/ScriptableObject/UIProvider/ResourceProvider.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ResourceProvider", menuName = "Scriptable Objects/ResourceProvider")]
+public class ResourceProvider : ProviderBase<ResourceCell>
+{
+    [SerializeField] private ResourceSO resourceDatabase;
+
+    public override Dictionary<Label, UIElementDataBase> CreateUIElementData()
+    {
+        return new()
+        {
+            { Label.CellName, new TextElementData(GetName(Label.CellName), "Resource") },
+            { Label.Location, new TextElementData(GetName(Label.Location), $"({System.XIndex}, {System.ZIndex})") },
+            {
+                Label.ResourceName,
+                new TextElementData(GetName(Label.ResourceName), resourceDatabase.GetInfo(System.ResourceType).Name)
+            }
+        };
+    }
+
+    public override void UpdateData(Label label, UIElementDataBase data)
+    {
+        if (label == Label.Location && data is TextElementData textData)
+        {
+            textData.Text = $"({System.XIndex}, {System.ZIndex})";
+        }
+    }
+}
