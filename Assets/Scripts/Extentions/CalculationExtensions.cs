8bb6f7d6f9f02b181b6da5c445c88636788a43e4
AoiTomiyama <TadoO.o2209@gmail.com>
2025-07-16 02:15:19 +0900

    [add] 計算補助用の拡張機能を追加

---
+++ Assets/Scripts/Extentions/CalculationExtensions.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public static class CalculationExtensions 
+{
+    /// <summary>
+    /// Vector3を正規化し、最も近い方向（前後左右）に変換します。
+    /// </summary>
+    /// <param name="dir"></param>
+    /// <returns></returns>
+    public static Vector3Int ToCardinalDirection(this Vector3 dir)
+    {
+        dir = dir.normalized;
+
+        if (Mathf.Abs(dir.x) > Mathf.Abs(dir.z))
+            return dir.x > 0 ? Vector3Int.right : Vector3Int.left;
+        else
+            return dir.z > 0 ? Vector3Int.forward : Vector3Int.back;
+    }
+}
