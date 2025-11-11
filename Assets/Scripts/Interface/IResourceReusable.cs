60aaecd507581d81ddc12edeb1f1ca49eacabbd2
AoiTomiyama <TadoO.o2209@gmail.com>
2025-11-12 00:29:58 +0900

    [add] summary追加

--- Assets/Scripts/Interface/IResourceReusable.cs
+++ Assets/Scripts/Interface/IResourceReusable.cs
@@ -2,5 +2,10 @@
 
 public interface IResourceReusable
 {
+    /// <summary>
+    /// リソースIDを再利用します。
+    /// </summary>
+    /// <param name="dir">アクセスされた方向</param>
+    /// <param name="id">再利用するリソースID</param>
     public void Reuse(Vector3Int dir, int id);
 }
